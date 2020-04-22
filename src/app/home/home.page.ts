3d658cd8628deb1c303e1b078261cafc44ce4133
LilianMichel <55470126+LilianMichel@users.noreply.github.com>
2020-04-22 15:54:57 -0600

    VALIDACIONES

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Estudiante } from '../models/estudiante';
+import { EstudianteService } from '../services/estudiante.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  public myForm: FormGroup;
+  public student: Estudiante;
+  constructor(private studentService: EstudianteService, private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      name: ['', [Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(150)])]],
+      controlnumber: ['', [Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(10)])]],
+      curp: ['', [Validators.compose([Validators.required, Validators.pattern("^(([A-ZÑ&]{4})([0-9]{2})([0][13578]|[1][02])(([0][1-9]|[12][\\d])|[3][01])([A-Z0-9]{3}))|" +
+      "(([A-ZÑ&]{4})([0-9]{2})([0][13456789]|[1][012])(([0][1-9]|[12][\\d])|[3][0])([A-Z0-9]{3}))|" +
+      "(([A-ZÑ&]{4})([02468][048]|[13579][26])[0][2]([0][1-9]|[12][\\d])([A-Z0-9]{3}))|" +
+      "(([A-ZÑ&]{4})([0-9]{2})[0][2]([0][1-9]|[1][0-9]|[2][0-8])([A-Z0-9]{3}))$")])]],
+      age: [0, [Validators.compose([Validators.required])]],
+      active: [false, [Validators.compose([Validators.required])]]
+    });
+  }
+
+  create() {
+    this.student = {
+      name: this.myForm.controls.name.value,
+      controlnumber: this.myForm.controls.controlnumber.value,
+      age: this.myForm.controls.age.value,
+      curp: this.myForm.controls.curp.value,
+      active: this.myForm.controls.active.value
+    }
+    this.studentService.createStudent(this.student);
+  }
 
 }
